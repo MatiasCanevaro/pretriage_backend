4b74077c7993ea698f2ba406576e8594ba1c0d6b
fcallero <fcallero@frba.utn.edu.ar>
2026-08-04 23:14:05 -0300

    arreglo pequeño error en CredencialResponse

--- src/main/java/com/pretriage/backend/mappers/MapperCredencial.java
+++ src/main/java/com/pretriage/backend/mappers/MapperCredencial.java
@@ -4,16 +4,15 @@ import com.pretriage.backend.controllers.dtos.CredencialResponse;
 import com.pretriage.backend.model.hospitales.Credencial;
 import lombok.RequiredArgsConstructor;
 
-
 @RequiredArgsConstructor
 public class MapperCredencial {
 
-    public static CredencialResponse toDTOResponse(Credencial credencial){
+    public static CredencialResponse toDTOResponse(Credencial credencial) {
         return new CredencialResponse(
                 credencial.getId(),
                 credencial.getNumeroAfiliado(),
                 credencial.getPlan(),
-                credencial.getFechaVencimiento()
-        );
+                credencial.getFechaVencimiento(),
+                credencial.getObraSocial().getNombre());
     }
 }
